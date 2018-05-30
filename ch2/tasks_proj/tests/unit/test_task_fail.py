901e2e9c2786f0a5a65f702af251e009c759333d
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2018-05-30 06:26:48 -0700

    Add example failing assert tests

---
+++ ch2/tasks_proj/tests/unit/test_task_fail.py
@@ -0,0 +1,16 @@
+"""Use the Task type to show test failures."""
+from tasks import Task
+
+
+def test_task_equality():
+    """Different tasks should not be equal."""
+    t1 = Task('sit there', 'joey')
+    t2 = Task('do something', 'hu')
+    assert t1 == t2
+
+
+def test_dict_equality():
+    """Different tasks compared as dicts should not be equal."""
+    t1_dict = Task('make sandwich', 'hu')._asdict()
+    t2_dict = Task('make sandwich', 'hv')._asdict()
+    assert t1_dict == t2_dict

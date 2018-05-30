b4621d506f0fa765feadfd69b728ced93e3cd88b
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2018-05-30 08:23:03 -0700

    Mark tests as expecting to fail

--- ch2/tasks_proj/tests/func/test_unique_id.py
+++ ch2/tasks_proj/tests/func/test_unique_id.py
@@ -17,7 +17,7 @@ def initialized_tasks_db(tmpdir):
     tasks.stop_tasks_db()
 
 
-@pytest.mark.skipif(
+@pytest.mark.xfail(
     tasks.__version__ < '0.2.0', reason='not supported until version 0.2.0')
 def test_unique_id_1():
     """Calling unique_id() twice should return different numbers."""
@@ -36,3 +36,17 @@ def test_unique_id_2():
     uid = tasks.unique_id()
     # Make sure it isn't in the list of existing ids
     assert uid not in ids
+
+
+@pytest.mark.xfail()
+def test_unique_id_is_a_duck():
+    """Demonstrate xfail."""
+    uid = tasks.unique_id()
+    assert uid == 'a duck'
+
+
+@pytest.mark.xfail()
+def test_unique_id_is_not_a_duck():
+    """Demonstrate xpass."""
+    uid = tasks.unique_id()
+    assert uid != 'a duck'

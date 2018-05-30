47f1aea1c6095d183fcd77f028fc04de783e3390
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2018-05-30 07:41:24 -0700

    Mark incorrect test as skip and add new test

--- ch2/tasks_proj/tests/func/test_unique_id.py
+++ ch2/tasks_proj/tests/func/test_unique_id.py
@@ -1,6 +1,7 @@
 import pytest
 
 import tasks
+from tasks import Task
 
 
 @pytest.fixture(autouse=True)
@@ -16,8 +17,21 @@ def initialized_tasks_db(tmpdir):
     tasks.stop_tasks_db()
 
 
-def test_unique_id():
+@pytest.mark.skip(reason='misunderstood the API')
+def test_unique_id_1():
     """Calling unique_id() twice should return different numbers."""
     id_1 = tasks.unique_id()
     id_2 = tasks.unique_id()
     assert id_1 != id_2
+
+
+def test_unique_id_2():
+    """unique_id() should return an unused id."""
+    ids = []
+    ids.append(tasks.add(Task('one')))
+    ids.append(tasks.add(Task('two')))
+    ids.append(tasks.add(Task('three')))
+    # Grab a unique id
+    uid = tasks.unique_id()
+    # Make sure it isn't in the list of existing ids
+    assert uid not in ids

c6774ef4843a4d9d6bb31560d365414d123ba57d
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2018-05-30 07:32:12 -0700

    Add misunderstood test calling unique_id method

---
+++ ch2/tasks_proj/tests/func/test_unique_id.py
@@ -0,0 +1,23 @@
+import pytest
+
+import tasks
+
+
+@pytest.fixture(autouse=True)
+def initialized_tasks_db(tmpdir):
+    """Connect to db before testing, disconnect after."""
+    # Setup: start db
+    tasks.start_tasks_db(str(tmpdir), 'tiny')
+
+    # This is where the testing happens.
+    yield
+
+    # Teardown: stop db
+    tasks.stop_tasks_db()
+
+
+def test_unique_id():
+    """Calling unique_id() twice should return different numbers."""
+    id_1 = tasks.unique_id()
+    id_2 = tasks.unique_id()
+    assert id_1 != id_2

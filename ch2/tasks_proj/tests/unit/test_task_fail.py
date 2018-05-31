9411d4a31edb92330901b3ace9869a3c4334ddab
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2018-05-31 07:45:54 -0700

    Mark expected test failures with xfail

--- ch2/tasks_proj/tests/unit/test_task_fail.py
+++ ch2/tasks_proj/tests/unit/test_task_fail.py
@@ -1,7 +1,10 @@
 """Use the Task type to show test failures."""
+import pytest
+
 from tasks import Task
 
 
+@pytest.mark.xfail()
 def test_task_equality():
     """Different tasks should not be equal."""
     t1 = Task('sit there', 'joey')
@@ -9,6 +12,7 @@ def test_task_equality():
     assert t1 == t2
 
 
+@pytest.mark.xfail()
 def test_dict_equality():
     """Different tasks compared as dicts should not be equal."""
     t1_dict = Task('make sandwich', 'hu')._asdict()

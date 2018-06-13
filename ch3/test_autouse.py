45b85d8f7e240d0509674028d933568f38420501
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2018-06-13 15:21:18 -0700

    Add tests showing fixture autouse flag

---
+++ ch3/test_autouse.py
@@ -0,0 +1,35 @@
+"""Demonstrate autouse fixtures."""
+
+import pytest
+import time
+
+
+@pytest.fixture(autouse=True, scope='session')
+def footer_session_scope():
+    """Report the time at the end of a session."""
+    yield
+    now = time.time()
+    print('--')
+    print('finished: {}'.format(
+        time.strftime('%d %b %X', time.localtime(now))))
+    print('-----------------​')
+
+
+@pytest.fixture(autouse=True)
+def footer_function_scope():
+    """Report time durations after each function."""
+    start = time.time()
+    yield
+    stop = time.time()
+    delta = stop - start
+    print('\ntest duration: {:0.3} seconds'.format(delta))
+
+
+def test_1():
+    """Simulate long-ish running test."""
+    time.sleep(1)
+
+
+def test_2():
+    """Simulate slightly longer test."""
+    time.sleep(1.23)

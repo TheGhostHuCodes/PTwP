0057760af048def75d6292cf120e1e1a2ccbcfd9
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2018-06-13 13:32:49 -0700

    Add test that throws an exception from inside a fixture

--- ch3/test_fixtures.py
+++ ch3/test_fixtures.py
@@ -22,3 +22,15 @@ def a_tuple():
 def test_a_tuple(a_tuple):
     """Demo the a_tuple fixture."""
     assert a_tuple[3]['bar'] == 32
+
+
+@pytest.fixture()
+def some_other_data():
+    """Raise an exception from fixture."""
+    x = 43
+    assert x == 42
+
+
+@pytest.mark.skip()
+def test_other_data(some_other_data):
+    pass
